09d6d42b2e341255722d94857bc210c417c6727d
Avineshwar Krishna Singh <100706343+AVINESHWAR-KRISHNA@users.noreply.github.com>
2023-06-23 17:10:57 +0530

    Update Java.js

--- Java.js
+++ Java.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const { createLogger, transports, format } = require('winston');
-const requestp = require('request-promise');
-const platformClient = require('purecloud-platform-client-v2');
 
 // Create the log directory
 const logDir = 'jslogs';
@@ -16,8 +14,7 @@ const logger = createLogger({
     format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
   ),
   transports: [
-    new transports.File({ filename: path.join(logDir, 'script.log') }),
-    new transports.Console()
+    new transports.File({ filename: path.join(logDir, 'script.log') })
   ]
 });
 
@@ -26,53 +23,68 @@ logger.on('error', (error) => {
   console.error('Error occurred while creating the log file:', error);
 });
 
-// Configure the PureCloud environment and access token
+const requestp = require('request-promise');
+const platformClient = require('purecloud-platform-client-v2');
 const client = platformClient.ApiClient.instance;
-client.setEnvironment(platformClient.PureCloudRegionHosts.us-west-2);
 
-// Export the contact list
-const exportContactList = async (contactListId, outputPath) => {
-  try {
-    logger.info('Exporting contact list...');
-    const outboundApi = new platformClient.OutboundApi();
-    const res = await outboundApi.getOutboundContactlistExport(contactListId, { download: 'false' });
-    const downloadUri = res.uri;
-    const data = await requestp({
-      uri: downloadUri,
-      headers: {
-        'Authorization': `bearer ${client.authData.accessToken}`
+// Configure the PureCloud environment and access token
+client.setEnvironment(platformClient.PureCloudRegionHosts.us-west-2);
+client.setAccessToken(client.authData.accessToken);
+
+const exportContactList = function exportContactList(contactListId, outputPath) {
+  logger.info('Exporting contact list...');
+
+  const outboundApi = new platformClient.OutboundApi();
+  outboundApi.getOutboundContactlistExport(contactListId, { download: 'false' })
+    .then((res) => {
+      const downloadUri = res.uri;
+      return requestp({
+        uri: downloadUri,
+        headers: {
+          'Authorization': `bearer ${client.authData.accessToken}`
+        }
+      });
+    })
+    .then((res) => {
+      logger.info('Export contents retrieved');
+
+      fs.writeFile(outputPath, res, (err) => {
+        if (err) {
+          logger.error('Failed to write export contents:', err);
+        } else {
+          logger.info('Contents exported successfully');
+        }
+      });
+    })
+    .catch((err) => {
+      logger.error('Failed to export contact list:', err);
+      if (err.body && err.body.code === 'no.available.list_export_uri') {
+        logger.info('Waiting for export...');
+        setTimeout(() => exportContactList(contactListId, outputPath), 5000);
       }
     });
-    await fs.promises.writeFile(outputPath, data);
-    logger.info('Contents exported successfully');
-  } catch (err) {
-    logger.error('Failed to export contact list:', err);
-    if (err.body && err.body.code === 'no.available.list_export_uri') {
-      logger.info('Waiting for export...');
-      setTimeout(() => exportContactList(contactListId, outputPath), 5000);
-    }
-  }
 };
 
 // Read command-line arguments or use default values
-const [clientId, clientSecret, contactListId, outputPath] = process.argv.slice(2);
+const clientId = process.argv[2] || '667becd3-4eef-4346-bcb8-608afe2fac96';
+const clientSecret = process.argv[3] || '2hieuwbroEmBcWysVd50-dyeb=2MDI-SAWILZRCISES';
+const contactListId = process.argv[4] || 'Cab8d178210ad97a6156fca924';
+const outputPath = process.argv[5] || '/path/to/file';
 
-// Run the script
-const runScript = async () => {
-  try {
-    // Configure the logger to also print to the console
-    logger.add(new transports.Console());
+// Configure the logger to also print to the console
+logger.add(new transports.Console());
 
-    // Login with client credentials and initiate contact list export
-    client.setAccessToken(await client.loginClientCredentialsGrant(clientId, clientSecret));
+// Login with client credentials and initiate contact list export
+client.loginClientCredentialsGrant(clientId, clientSecret)
+  .then(() => {
+    logger.info('Logged in successfully');
     const outboundApi = new platformClient.OutboundApi();
-    await outboundApi.postOutboundContactlistExport(contactListId);
-
-    await exportContactList(contactListId, outputPath);
-    logger.info('Script execution completed.');
-  } catch (err) {
+    return outboundApi.postOutboundContactlistExport(contactListId);
+  })
+  .then(() => {
+    logger.info('Contact list export initiated');
+    exportContactList(contactListId, outputPath);
+  })
+  .catch((err) => {
     logger.error('Failed to log in or initiate contact list export:', err);
-  }
-};
-
-runScript();
+  });
