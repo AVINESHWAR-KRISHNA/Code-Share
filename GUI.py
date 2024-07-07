22f5ff18f5df489720f49a2af46e4b23d20226f4
Avineshwar Krishna Singh <100706343+AVINESHWAR-KRISHNA@users.noreply.github.com>
2024-07-07 18:27:14 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -60,19 +60,13 @@ window.resizable(False, False)
 # animated widget
 animated_panel = SlidePanel(window, 1.0, 0.75)
 
-ctk.CTkButton(animated_panel, text = 'Genesys Load', fg_color=("#DB3E39", "#821D1A"), corner_radius = 0).place(relx = 0.5, rely = 0.3, anchor = 'center')
-ctk.CTkButton(animated_panel, text = 'Genesys Export', fg_color=("#DB3E39", "#821D1A"), corner_radius = 0).place(relx = 0.5, rely = 0.4, anchor = 'center')
-ctk.CTkButton(animated_panel, text = 'i3 Databases', fg_color=("#DB3E39", "#821D1A"), corner_radius = 0).place(relx = 0.5, rely = 0.5, anchor = 'center')
+ctk.CTkButton(animated_panel, text = ' Load', fg_color=("#DB3E39", "#821D1A"), corner_radius = 0).place(relx = 0.5, rely = 0.3, anchor = 'center')
+ctk.CTkButton(animated_panel, text = ' Export', fg_color=("#DB3E39", "#821D1A"), corner_radius = 0).place(relx = 0.5, rely = 0.4, anchor = 'center')
+ctk.CTkButton(animated_panel, text = ' Databases', fg_color=("#DB3E39", "#821D1A"), corner_radius = 0).place(relx = 0.5, rely = 0.5, anchor = 'center')
 
 button = ctk.CTkButton(window, text = 'Get Started', command = animated_panel.animate)
 button.place(relx = 0.5, rely = 0.95, anchor = 'center')
 
-Note = ctk.CTkTextbox(window, fg_color = 'transparent', width=530, height=450, corner_radius= 5, border_color='darkgray', border_width=3, font=("Times New Roman Greek", 14), wrap=ctk.WORD,text_color="black")
-Note.insert('end', '''Genesys Load ::\nTo truncate the Genesys Contact List and re load the i3 db data into the Genesys Contact List.\n\n                                                            OR\nReload the partial data without truncating the Genesys Contact List.
-                  \n\nGenesys Export ::\nThis will re export the data from the Genesys Contact List to the FTP location.\n\n                                                            OR\nIf the csv File is already exported then to insert the data into Raw table.
-                  \n\ni3 Databases ::\nIf the ask is to Truncate the Genesys Contact List and re load the i3 db data into the Genesys Contact List.\nThen you have to update the value of CqNotes as'Loaded' in i3 Database Callqueue Table.\n''')
-Note.configure(state="disabled")
-Note.place(relx = 0.35, rely = 0.46, anchor = 'center')
-Note.focus_set()
+
 # run
 window.mainloop()
